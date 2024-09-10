116b423e3d7ef586f61050d36aadd78965152a2e
Bart Roosen <84047662+BartRoosenCampus@users.noreply.github.com>
2024-09-10 10:54:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// index.php
+declare(strict_types=1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+</body>
+</html>
